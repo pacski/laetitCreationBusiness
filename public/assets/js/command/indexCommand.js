d8eb1556b6446bef1c7be69e7c6cca29654922cd
Pacski <remy.pachocinski@devinci.fr>
2020-04-06 00:49:17 +0200

    test

--- public/assets/js/command/indexCommand.js
+++ public/assets/js/command/indexCommand.js
@@ -56,8 +56,7 @@ for (let index = 0; index < command.nbCommand; index++) {
                 break;
         }
         let year = date.getFullYear()
-        console.log(month)
-        console.log(day +' '+ month +' '+ year)
+    
 
         // ------------
         $('.number-command').html(data.number)
